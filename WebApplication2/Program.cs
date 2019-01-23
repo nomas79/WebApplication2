4253656b28b59e46757bf6b2aa4592f2770861c1
Macias <macias.bensimhon@namely.com>
2019-01-23 09:32:12 -0500

    test5

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -15,7 +15,7 @@ namespace WebApplication2
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
-
+            
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
